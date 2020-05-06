4bf9e633ccaf75b2845e9fe2e5c12e2bc2f13fb3
Hugo Vargas <vargas88hugo@gmail.com>
2020-05-06 09:13:40 -0500

    [Fix] Corrected misspelled name class

--- RouletteApi/Models/RouletteDatabaseSettings.cs
+++ RouletteApi/Models/RouletteDatabaseSettings.cs
@@ -2,14 +2,14 @@ namespace RouletteApi.Models
 {
   public class RouletteDatabaseSettings : IRouletteDatabaseSettings
   {
-    public string RoulletesCollectionName { get; set; }
+    public string RoulettesCollectionName { get; set; }
     public string ConnectionString { get; set; }
     public string DatabaseName { get; set; }
   }
 
   public interface IRouletteDatabaseSettings
   {
-    string RoulletesCollectionName { get; set; }
+    string RoulettesCollectionName { get; set; }
     string ConnectionString { get; set; }
     string DatabaseName { get; set; }
   }
