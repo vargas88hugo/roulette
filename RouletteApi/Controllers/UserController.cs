fc304e3d30f329cd09bdcc1ff728a9aec3c9bc8f
Hugo Vargas <vargas88hugo@gmail.com>
2020-05-09 23:53:34 -0500

    [Feature] Tested authorize and works!

--- RouletteApi/Controllers/UserController.cs
+++ RouletteApi/Controllers/UserController.cs
@@ -1,12 +1,15 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using RouletteApi.Interfaces;
 using RouletteApi.Models.Entities;
 
 namespace RouletteApi.Controllers
 {
+  [Authorize]
   [Route("api/v2/[controller]")]
+  [ApiController]
   public class UserController : ControllerBase
   {
     private readonly IUserService _userService;
