b620967c97c21be027a6c5b6fa14291558482a32
Hugo Vargas <vargas88hugo@gmail.com>
2020-05-10 13:28:27 -0500

    [Update] Refactore code and implemented some conditions like colors

--- RouletteApi/Models/BetModel.cs
+++ RouletteApi/Models/BetModel.cs
@@ -5,14 +5,20 @@ namespace RouletteApi.Models
   public class BetModel
   {
     [Required(ErrorMessage = "Color is obligatory")]
+    [RegularExpression("^(((b|B)lack)|((r|R)ed))$",
+    ErrorMessage = "Please enter a red or black color")]
     public string Color { get; set; }
+
     [Required(ErrorMessage = "Money is obligatory")]
     [Range(0, 10000, ErrorMessage = "Bet amount must be between 0 and 1000")]
     public int Money { get; set; }
+
     [Required(ErrorMessage = "Number bet is obligatory")]
     [Range(0, 36, ErrorMessage = "Number bet must be between 0 and 36")]
     public int Number { get; set; }
+
     [Required(ErrorMessage = "Roulette Id is obligatory")]
+    [StringLength(25)]
     public string RouletteId { get; set; }
   }
 }
--- RouletteApi/Models/RegisterModel.cs
+++ RouletteApi/Models/RegisterModel.cs
@@ -4,19 +4,16 @@ namespace RouletteApi.Models
 {
   public class RegisterModel
   {
-    [
-      Required(ErrorMessage = "Username is obligatory"),
-      MaxLength(25, ErrorMessage = "No more than 25 characters")
-    ]
+    [Required(ErrorMessage = "Username is obligatory")]
+    [MaxLength(25, ErrorMessage = "No more than 25 characters")]
     public string Username { get; set; }
-    [
-      Required(ErrorMessage = "Password is obligatory")
-    ]
+
+    [Required(ErrorMessage = "Password is obligatory")]
+    [MaxLength(25, ErrorMessage = "No more than 25 characters")]
     public string Password { get; set; }
-    [
-      Required(ErrorMessage = "Money is obligatory"),
-      Range(1, int.MaxValue, ErrorMessage = "Please enter a money bigger than 1")
-    ]
+
+    [Required(ErrorMessage = "Money is obligatory")]
+    [Range(1, int.MaxValue, ErrorMessage = "Please enter a money bigger than 1")]
     public int Money { get; set; }
   }
 }
