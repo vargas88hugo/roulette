23bac4bce51fd0b95974d263a5b608f0bbae14a9
Hugo Vargas <vargas88hugo@gmail.com>
2020-05-10 12:46:39 -0500

    [Update] Refactore code in Startup.cs fileQ

--- RouletteApi/Helpers/StartupHelper.cs
+++ RouletteApi/Helpers/StartupHelper.cs
@@ -1,9 +1,37 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.DependencyInjection;
-using RouletteApi.Models;
+using Microsoft.IdentityModel.Tokens;
+using RouletteApi.Interfaces;
 
 namespace RouletteApi.Helpers
 {
   public class StartupHelper
   {
+    public static void ValidateToken(JwtBearerOptions options, byte[] key)
+    {
+      options.Events = new JwtBearerEvents
+      {
+        OnTokenValidated = context =>
+        {
+          var userService = context.HttpContext.RequestServices
+            .GetRequiredService<IUserRepository>();
+          var userId = context.Principal.Identity.Name;
+          var user = userService.GetUserById(userId);
+          if (user == null)
+            context.Fail("Unauthorized");
+          return Task.CompletedTask;
+        }
+      };
+      options.RequireHttpsMetadata = false;
+      options.SaveToken = true;
+      options.TokenValidationParameters = new TokenValidationParameters
+      {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+      };
+    }
   }
 }
--- RouletteApi/Startup.cs
+++ RouletteApi/Startup.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Text;
-using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
@@ -8,7 +7,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
 using RouletteApi.Helpers;
 using RouletteApi.Interfaces;
 using RouletteApi.Models;
@@ -53,47 +51,19 @@ namespace RouletteApi
         x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
         x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
       })
-      .AddJwtBearer(x =>
-      {
-        x.Events = new JwtBearerEvents
-        {
-          OnTokenValidated = context =>
-                {
-                  var userService = context.HttpContext.RequestServices.GetRequiredService<IUserRepository>();
-                  var userId = context.Principal.Identity.Name;
-                  var user = userService.GetUserById(userId);
-                  if (user == null)
-                    context.Fail("Unauthorized");
-                  return Task.CompletedTask;
-                }
-        };
-        x.RequireHttpsMetadata = false;
-        x.SaveToken = true;
-        x.TokenValidationParameters = new TokenValidationParameters
-        {
-          ValidateIssuerSigningKey = true,
-          IssuerSigningKey = new SymmetricSecurityKey(key),
-          ValidateIssuer = false,
-          ValidateAudience = false
-        };
-      });
+      .AddJwtBearer(options => StartupHelper.ValidateToken(options, key));
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
       if (env.IsDevelopment())
-      {
         app.UseDeveloperExceptionPage();
-      }
 
       app.UseHttpsRedirection();
       app.UseRouting();
       app.UseAuthentication();
       app.UseAuthorization();
-      app.UseEndpoints(endpoints =>
-      {
-        endpoints.MapControllers();
-      });
+      app.UseEndpoints(endpoints => endpoints.MapControllers());
     }
   }
 }
